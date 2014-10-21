a3150613a0176bbc672c6926902bf6e1915ded42
Dustin Getz <dustin@Dustins-MacBook-Pro.local>
2014-10-21 13:56:38 -0400

    Unit tests can see React and render a simple component

--- test/testCursor.js
+++ test/testCursor.js
@@ -2,19 +2,35 @@
 
 var ReactTestUtils;
 
+function buildStatefulComponent(initialState) {
+  return React.createClass({
+    getInitialState: function () {
+      return initialState;
+    },
+    render: function () {
+      return React.DOM.pre({}, JSON.stringify(this.state));
+    }
+  });
+}
+
+
 describe("Label Test",function(){
-    beforeEach(function() {
-        ReactTestUtils = React.addons.TestUtils;
-    });
+  beforeEach(function() {
+    ReactTestUtils = React.addons.TestUtils;
+  });
 
+  it("do the unit tests work", function () {
+    expect(true).toBe(true);
+  });
 
-    it("Check Text Assignment", function () {
-        expect(true).toBe(true);
-        // var label = <Label>Some Text We Need for Test</Label>;
-        // ReactTestUtils.renderIntoDocument(label);
-        // expect(label.refs.p).toBeDefined();
-        // expect(label.refs.p.props.children).toBe("Some Text We Need for Test")
-    });
+  it("can we make an instance of a react cmp and get at the state", function () {
+    var MyCmp = buildStatefulComponent({ a: 42 });
+    var cmp = MyCmp({});
+    ReactTestUtils.renderIntoDocument(cmp);
+    expect(true).toBe(true);
+    // expect(label.refs.p).toBeDefined();
+    // expect(label.refs.p.props.children).toBe("Some Text We Need for Test")
+  });
 
     // it("Click", function () {
     //     var label  = <Label>Some Text We Need to Test</Label>;
